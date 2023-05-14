f03edabeaf035a692f2d9d7d74d672ba8effdc0c
Snehil Tiwari <86239745+tiwarisnehil@users.noreply.github.com>
2023-05-14 12:08:49 +0530

    Add files via upload

---
+++ src/main/java/github/leetcode/strings/BinaryTreeInorderTraversal.java
@@ -0,0 +1,29 @@
+package Trees;
+}
+public class TreeNode {
+    int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer>list=new ArrayList<>();
+        helper(list,root);
+        return list;
+    }
+    public void helper(List<Integer>list,TreeNode root){
+        if(root==null){
+            return;
+        }
+        helper(list,root.left);
+        list.add(root.val);
+        helper(list,root.right);
+    }
+}
